4aaf1cb62e88e7c8fe943c5f27009aaf95c9c642
HopedCrown <258532179+HopedCrown@users.noreply.github.com>
2026-03-12 08:00:44 -0500

    Update addr.js

--- addr.js
+++ addr.js
@@ -2,15 +2,23 @@
       async function getVisitorIP() {
         try {
         // Send a request to the ipify API
-          const response = await fetch('https://api.ipify.org?format=json');
+          fetch('https://api.example.com/data')
+                .then(response => response.json()) // This handles the JSON parsing
+                .then(data => {
+                    console.log(data); // 'data' is already a JavaScript object
+                      const ipHTML = document.getElementById("ip");
+                      ipHTML.innerHTML = "IP:" + data.ip;
+          })
+          .catch(error => {
+              console.error('Error fetching or parsing JSON:', error);
+          });
+
         
         // Parse the JSON response
           const data = await response.json();
         
         // The IP address is in the 'ip' field
           console.log('Visitor IP Address:', data.ip);
-          const ipHTML = document.getElementById("ip");
-          ipHTML.innerHTML = "IP:" + data;
         
         
         // You can then use this IP address in your website logic
